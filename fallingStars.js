f0355d1393dda58d4f9fa3f7849409557dcacd2b
lstigale <leahstigale@gmail.com>
2018-08-12 20:14:27 -0700

    Added stars in the background

--- fallingStars.js
+++ fallingStars.js
@@ -1,4 +1,5 @@
 var amountOfStars = Math.floor(Math.random()*(75 - 40 + 1) + 40);
+var amountOfBackStars = Math.floor(Math.random()*(300 - 200 + 1) + 200);
 
 function starPlacement() {
 
@@ -6,15 +7,27 @@ var starDatabase = [];
 var divNum = [];
 var mainContainer = document.getElementById('sky');
 
+var backStars = [];
+var backStarDivNum = [];
+
 for (var i = 0; i < amountOfStars; i++) {
        divNum[i] = document.createElement('div');
        divNum[i].style.left = (Math.random()*100+1) + "%";
        divNum[i].style.top = (Math.random()*100+1) + "%";
        divNum[i].className = 'star';
-       divNum[i].id = (i + 1);
+       // divNum[i].id = (i + 1);
        mainContainer.appendChild(divNum[i]);
        starDatabase.push(i + 1);
      }
+
+for (var i = 0; i < amountOfBackStars; i++) {
+      backStarDivNum[i] = document.createElement('div');
+      backStarDivNum[i].style.left = (Math.random()*100+1) + "%";
+      backStarDivNum[i].style.top = (Math.random()*100+1) + "%";
+      backStarDivNum[i].className = 'backStar';
+      mainContainer.appendChild(backStarDivNum[i]);
+      backStars.push(i + 1);
+    }
 }
 
 var color = 0;
